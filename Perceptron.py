89b0c478aa3604f5ff7497e660ef4cfdbb1fd7e6
Danilo Azevedo <zedanilo161@gmail.com>
2018-07-20 20:06:24 -0300

    funcionando meia boca

---
+++ Perceptron.py
@@ -0,0 +1,233 @@
+# importa o plugin numpy que manipula as matrizes de modo mais facil no python
+import numpy as np
+
+# Estou criando um array de arrays, é como se fosse uma matriz, porém dentro deste array eu tenho outro(vetores)
+# linha deste array, ou seja cada elemnto deste array x, contem um vetor de amostras de X, é como se em cada linha
+# correspondesse a X1, X2, X3, X....X9.
+#Cada vetor(linha) deste array(matriz) é a representação em pixel de um numero de zero a 9, estou dizendo pra minha
+#rede que o numero O, 2, 3, 5 etc são representados na composição abaixo:
+
+x = np.array([[1, -1, 1, 1, 1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, 1, 1, 1, -1],
+              [1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, 1, 1, 1, -1],
+              [1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, -1],
+              [1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, -1],
+              [1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1],
+              [1, -1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, -1],
+              [1, -1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1],
+              [1, -1, 1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1],
+              [1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1],
+              [1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, -1]])
+
+
+# No Y desjado eu estou dizendo oomo deve estar ativo (1) o resultado da minha função para cada numero, ou seja
+# cada linha do array abaixo representa um numero de 0 a 9 respectivamente, quando o numero eu quero que minha rede
+# retorne o numero 9, por exemplo, significa que só vai estar ativa no ultimo digito e sendo intaiva (-1) par todo o
+#restante.
+yd = np.array([[1, -1, -1, -1, -1, -1, -1, -1, -1, -1], # 0
+               [-1, 1, -1, -1, -1, -1, -1, -1, -1, -1], # 1
+               [-1, -1, 1, -1, -1, -1, -1, -1, -1, -1], # 2
+               [-1, -1, -1, 1, -1, -1, -1, -1, -1, -1], # 3
+               [-1, -1, -1, -1, 1, -1, -1, -1, -1, -1], # 4
+               [-1, -1, -1, -1, -1, 1, -1, -1, -1, -1], # 5
+               [-1, -1, -1, -1, -1, -1, 1, -1, -1, -1], # 6
+               [-1, -1, -1, -1, -1, -1, -1, 1, -1, -1], # 7
+               [-1, -1, -1, -1, -1, -1, -1, -1, 1, -1], # 8
+               [-1, -1, -1, -1, -1, -1, -1, -1, -1, 1]]) # 9
+
+#Gerar W aleatorio de forma elegante
+#gera uma matriz 10 linhas com 26 colunas (ou neste caso 10 vetores com 26 elementos) contendo apenas 2 valores (0 ou 1)
+w = np.random.randint(2, size=(10, 26))
+
+#troca a matriz(array) w aonde o valor dentro dela for zero por -1
+np.place(w, w==0, [-1])
+
+# Geração "na mao" do W, que é a sinapse aleatória usada para treinar minha rede.
+
+# w = np.array([[1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, 1, -1, -1, -1],
+#               [1, -1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1],
+#               [-1, 1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1],
+#               [1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, 1, 1, 1, -1],
+#               [-1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, -1, -1, 1, 1, 1],
+#               [1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1],
+#               [-1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1],
+#               [1, -1, 1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, 1, -1, 1],
+#               [-1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+#               [-1, -1, 1, 1, -1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1]])
+
+
+#Variaveis de apoio
+# O erro médio é resetado a cada rodada do meu loop, então a variavel erroMedioEpocaGeral armazenda esse erro sem perder
+# as informações
+erroMedioEpocaGeral = np.array([[]])
+
+# minha constante Sigma
+sigma = 0.5
+
+# Minha constante para erro maximo que aceito em minha rede
+erroMaximo = 0.04
+
+# Epoca inicial
+epoca = 1
+
+# Rodada / instante inicial
+instante = 0
+
+# Auto explicativo
+escolha = 6
+
+while escolha != 3:
+    print("1 - Treinar")
+    print("2 - Testar")
+    print("3 - Sair")
+    escolha = int(input("Choose your Destiny \n"))
+
+    if escolha == 1:
+        if epoca > 1:
+            instante = 0
+        while instante < len(x):
+
+            # Inicia todos os arrays que irei usar de forma vazai
+            vetorSomado = np.array([[]])
+            yEncontrado = np.array([[]])
+            eroou = np.array([[]])
+            erroMedioEpoca = np.array([[]])
+            deltaAux = np.array([[]])
+            deltaW = np.array([[]])
+            if instante == 0:
+                erroMedioEpocaGeral = np.array([[]])
+
+            # treinamento ------------------------------------------------------------------
+
+            # o Array V vai receber o resultado da multiplicação de x por W
+            # Aqui eu fiz um for in line, mas nao gosto de usar assim posi nao acho que fica algo facil
+            # de ler ou interpretar. Cada linha de V ira receber o resultado da multiplicação linha a linha de W por
+            # X "fixo" em 0 a 10.
+            v = np.array([w * x[instante] for w in w])
+
+            # Nesta fase eu preciso do valor somado de cada uma das linhas acima, então eu faço um novo for
+            # onde o vetorSomado irá ser construido com os valores da soma de cada linha de v
+            i = 0
+            for apoio in v:
+                vetorSomado = np.append(vetorSomado, [np.sum(v[i])])
+                i = i + 1
+
+            # Aqui eu aplico a função para verificar de os valores são positivos ou negativos
+            # para cada caso o novo vetor Y recebera os valores 1 ou -1
+            # Encontrar Y desejado -------------------------------------------------------------
+            i = 0
+            for apoio in vetorSomado:
+                if vetorSomado[i] > 0:
+                    yEncontrado = np.append(yEncontrado, [1])
+                else:
+                    yEncontrado = np.append(yEncontrado, [-1])
+                i = i + 1
+
+            # Agora a rede irá calcular o erro, ou seja, verificar quanto errado foi o valor do
+            # Vetor Y que encontramos acima em comparação ao valor do Y desejado qeu definimos lá no começo
+
+            # calcular erro -------------------------------------------------------------------
+            i = 0
+            for apoio in yEncontrado:
+                formulaDoErro = yd[i][instante] - (yEncontrado)[i]
+                eroou = np.append(eroou, [formulaDoErro])
+                i = i + 1
+
+            # O calculo do erro médio para esta rodada é realizado agora
+            # Calcular erro médio -------------------------------------------------------------------
+            somaDosErros = np.sum(eroou)
+            totalDeerros = len(eroou)
+            calculaerroMedioEpoca = somaDosErros / totalDeerros
+            erroMedioEpoca = np.append(erroMedioEpoca, [calculaerroMedioEpoca])
+            erroMedioEpocaGeral = np.append(erroMedioEpocaGeral, [erroMedioEpoca])
+
+
+            # Atualizar Sinapse -------------------------------------------------------------------
+            # O For a seguir calcula separadamente o valor de sigma multiplicado pelos  erros e armazenda em SigamaXerro
+            # em seguida uma variavel auxiliar sigmaXerro e multiplica pelo X
+            # por ultimo o array deltaW armazenda os valores de deltaAux a cada ciclo do for
+            i = 0
+            tamanhodeW = len(w)
+            for apoio in range(0, tamanhodeW):
+                sigmaXerro = sigma * eroou[i]
+                deltaAux = sigmaXerro * x[instante]
+                deltaW = np.append(deltaW, [deltaAux])
+                i = i + 1
+
+            # Gambiarra para dividir o array
+            deltaW = (np.split(deltaW, len(w)))
+
+            # Soma o vetor W original com o valor de Delta W
+            w = (np.add(w, deltaW))
+
+            # Indica o final desta rodada
+            instante = instante + 1
+
+            if instante == len(x):
+                emqQuadrado = (erroMedioEpocaGeral * erroMedioEpocaGeral)
+                emq = (np.sum(emqQuadrado)) / len(x)
+                print("Erro medio Quadratico da Epoca N: ", epoca)
+                print('= ', emq)
+                print("")
+                epoca = epoca + 1
+
+                if emq > erroMaximo:
+                    # epoca = epoca + 1
+                    instante = 0
+                    print("EMQ maior que erro maximo aceitavel, nova rodada.")
+
+
+
+
+    elif escolha == 2:
+        debugar = int(input("DEBUGAR? 0 ..... 9"))
+        if debugar == 0:
+            vetorUsuario = np.array([[1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1, -1, -1, -1, 1, -1, 1, -1, -1, -1]])
+        if debugar == 1:
+            vetorUsuario = np.array([[1, -1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1]])
+        if debugar == 2:
+            vetorUsuario = np.array([[-1, 1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1]])
+        if debugar == 3:
+            vetorUsuario = np.array([[1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, -1, -1, -1, 1, 1, -1, -1, 1, 1, 1, -1]])
+        if debugar == 4:
+            vetorUsuario = np.array([[-1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, -1, 1, -1, -1, -1, 1, 1, 1]])
+        if debugar == 5:
+            vetorUsuario = np.array([[1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1]])
+        if debugar == 6:
+            vetorUsuario = np.array([[-1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1]])
+        if debugar == 7:
+            vetorUsuario = np.array([[1, -1, 1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, 1, -1, 1]])
+        if debugar == 8:
+            vetorUsuario = np.array([[-1, -1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1]])
+        if debugar == 9:
+            vetorUsuario = np.array([[-1, -1, 1, 1, -1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1]])
+
+        # 7
+        # vetorUsuario = np.array([[]])
+        # i = 1
+        # for apoio in range(0, 25):
+        #     valorDeX = int(input("Informe o valor de x%d " % i))
+        #     vetorUsuario = np.append(vetorUsuario, [valorDeX])
+        #     i = i+1
+        # treinamento ------------------------------------------------------------
+
+        instante = 0
+        vetorSomado = np.array([[]])
+        yEncontrado = np.array([[]])
+        # treinamento ------------------------------------------------------------
+        v = np.array([w * vetorUsuario for w in w])
+        i = 0
+        for apoio in v:
+            vetorSomado = np.append(vetorSomado, [np.sum(v[i])])
+            i = i + 1
+        # Encontrar Y  -------------------------------------------------------------
+        i = 0
+        print(vetorSomado)
+        for apoio in vetorSomado:
+            if vetorSomado[i] > 0:
+                yEncontrado = np.append(yEncontrado, [1])
+            else:
+                yEncontrado = np.append(yEncontrado, [-1])
+            i = i + 1
+        print(yEncontrado)
+        numero = yEncontrado.tolist().index(1)+1
+        print("o Vetor informado correponde ao numero: ", numero)
